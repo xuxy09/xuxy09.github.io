cdc941ea0ab44fd0b798490eb31b3a6401ac7128
xuxy09 <251552202@qq.com>
2023-11-16 23:20:07 +0800

    arxiv papers

--- cut_image.py
+++
@@ -1,7 +0,0 @@
-import imageio
-
-name_list = ['images/tip20denoise_after.png', 'images/tip20denoise_before.png']
-for name in name_list:
-    img = imageio.imread(name)
-    img = img[0:img.shape[0], 0:img.shape[0]]
-    imageio.imwrite(name[:-4]+'_cut.png', img)
